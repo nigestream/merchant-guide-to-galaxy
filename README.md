1913da5e358ca091ff3db7e18b70bf5ae5c5573e
chenshigang <nigestream.chen@orderplus.com>
2020-05-15 11:35:02 +0800

    fix unit test

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ merchant-to-galaxy
 ![1](merchant.png)
 * jdk1.8
 * 罗马符号组合的规则用正则表达式表示
-* 完整的一段话是Paragraph, 每一行为sentence
+* 程序输入为完整的一段话即Paragraph, 每一行为sentence, 恢复的内容为answer
 * sentence 的类型有：assigned、credit、how many、how much
 * 罗马字符的计算是从后往前算，如果后面的大于前面的就用大减小，否则就是相加
 
--- src/test/com/galaxy/merchant/SentenceServiceTest.java
+++ src/test/com/galaxy/merchant/SentenceServiceTest.java
@@ -1,23 +1,25 @@
 package test.com.galaxy.merchant;
 
+import com.galaxy.merchant.entity.Sentence;
 import com.galaxy.merchant.enums.SentenceType;
 import com.galaxy.merchant.service.SentenceService;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class SentenceServiceTest {
-//    @Test
-//    public void testGetSentenceType() {
-//        String sentence = "how much is pish glob prok ?";
-//        String sentence1 = "how many is pish glob prok ?";
-//        String sentence2 = "glob prok Gold is 57800 Credits";
-//        String sentence3 = "tegj is M";
-//        String sentence4 = "how many Credits is glob prok Silver ?";
-//        SentenceService sentenceService = new SentenceService();
-//        Assert.assertEquals(SentenceType.QUESTION_HOW_MUCH, sentenceService.getSentenceType(sentence));
-//        Assert.assertEquals(SentenceType.NOMATCH, sentenceService.getSentenceType(sentence1));
-//        Assert.assertEquals(SentenceType.CREDITS, sentenceService.getSentenceType(sentence2));
-//        Assert.assertEquals(SentenceType.ASSIGNED, sentenceService.getSentenceType(sentence3));
-//        Assert.assertEquals(SentenceType.QUESTION_HOW_MANY, sentenceService.getSentenceType(sentence4));
-//    }
+    @Test
+    public void testGetSentenceType() {
+        Sentence sentence = new Sentence("how much is pish glob prok ?");
+        Sentence sentence1 = new Sentence("how many is pish glob prok ?");
+        Sentence sentence2 = new Sentence("glob prok Gold is 57800 Credits");
+        Sentence sentence3 = new Sentence("tegj is M");
+        Sentence sentence4 = new Sentence("how many Credits is glob prok Silver ?");
+
+
+        Assert.assertEquals(SentenceType.QUESTION_HOW_MUCH, sentence.getSentenceType());
+        Assert.assertEquals(SentenceType.NOMATCH, sentence1.getSentenceType());
+        Assert.assertEquals(SentenceType.CREDITS, sentence2.getSentenceType());
+        Assert.assertEquals(SentenceType.ASSIGNED, sentence3.getSentenceType());
+        Assert.assertEquals(SentenceType.QUESTION_HOW_MANY, sentence4.getSentenceType());
+    }
 }
